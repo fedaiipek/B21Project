f114ad3c60fee7b4b8167efa0d4a41215fc8d09d
fedaiipek <fedaiipek55@gmail.com>
2020-11-10 11:19:34 -0600

    initial commit continued

--- src/main.java
+++ src/main.java
@@ -1,7 +1,9 @@
-import java.sql.SQLOutput;
+
 
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello");
+        // B21 is good at intellij
+        System.out.println("world");
     }
 }
